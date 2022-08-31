9492f331a853f40524bc5596f83eed804ddea723
mai tarek <>
2022-08-31 16:52:38 +0200

    some translations

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -3,7 +3,23 @@
 return [
 
 	'edit admin' => 'Edit data',
-
+	'remember me' => 'remember me',
+	'login' => 'login',
+	'enter email' => 'enter email',
+	'enter password' => 'enter password',
+	'email' => 'Email',
+	'password' => 'Password',
+	'sign in to dashboard' => 'Signin to dashboard',
+	'dashboard' => 'Dashboard',
+	'logout' => 'Logout',
+	'setting' => 'Setting',
+	'all roles' => 'Roles',
+	'admins' => 'Admins',
+	'users' => 'Users',
+	'services' => 'Services',
+	'portfolios' => 'Portfolios',
+	'contacts' => 'Contacts',
+	'subscribes' => 'Subscribes',
 ]
 
 ?>
